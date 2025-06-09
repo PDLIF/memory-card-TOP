f9ff45d5f9582a1394edecf96120030566c21311
astuarykig <akvsevolod@icloud.com>
2025-06-09 15:11:02 +0500

    add tilt library and more styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Tilt from 'react-parallax-tilt';
 import '../style/Card.css';
 
 const Card = ({ card, onCardClick }) => {
 
     return (
-        <div className="card" onClick={() => onCardClick(card)}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-        </div>
+        <Tilt className="custom-tilt" tiltReverse={true} glareEnable={true} glareBorderRadius={'1rem'}>
+            <div className="card" onClick={() => onCardClick(card)}>
+                <img src={card.image} alt="" />
+                <p>{card.name}</p>
+            </div>
+        </Tilt>
     )
 }
 
